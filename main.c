341259814fe4d02168b52b6f9db96b14cf39f380
NitchayaninT <146054441+NitchayaninT@users.noreply.github.com>
2023-09-27 10:19:48 +0700

    week 2 egci113

--- main.c
+++ main.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//add 2 times together (in h and m)
 int main()
 {
-    int w,h,a;
-    printf("Insert width : ");
-    //insert width of a triangle
-    scanf("%d",&w);
-    printf("Insert height : ");
-    //insert height of a triangle
-    scanf("%d",&h);
-    //calculate the area
-    a=w*h;
-    printf("The area of a rectangle is %d",a);
-    //print area
+    int h1,m1,h2,m2,s1,s2,totalsec;
+    printf("Insert time 1 (h:m:s) : ");
+    scanf("%d:%d:%d",&h1,&m1,&s1);
+    printf("Insert time 2 (h:m:s) : ");
+    scanf("%d:%d:%d",&h2,&m2,&s2);
+    totalsec=(h1*3600)+(h2*3600)+(m1*60)+(m2*60)+s1+s2;
+    printf("The sum of the 2 times is %d:%02d:%02d\n",totalsec/3600,(totalsec%3600)/60,(totalsec%3600)%60);
     return 0;
 }
