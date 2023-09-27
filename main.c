2b3d4d5e36ee7736b6fe4ee706ac0f09009c6a79
NitchayaninT <146054441+NitchayaninT@users.noreply.github.com>
2023-09-27 10:32:57 +0700

    week 2 assignment

--- main.c
+++ main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-//add 2 times together (in h and m)
+
 int main()
 {
-    int h1,m1,h2,m2,s1,s2,totalsec;
-    printf("Insert time 1 (h:m:s) : ");
-    scanf("%d:%d:%d",&h1,&m1,&s1);
-    printf("Insert time 2 (h:m:s) : ");
-    scanf("%d:%d:%d",&h2,&m2,&s2);
-    totalsec=(h1*3600)+(h2*3600)+(m1*60)+(m2*60)+s1+s2;
-    printf("The sum of the 2 times is %d:%02d:%02d\n",totalsec/3600,(totalsec%3600)/60,(totalsec%3600)%60);
+    int stone1,lb1,oz1,stone2,lb2,oz2;
+    printf("Input 1st weight:");
+    scanf("%d,%d,%d",&stone1,&lb1,&oz1);
+    printf("Input 2nd weight:");
+    scanf("%d,%d,%d",&stone2,&lb2,&oz2);
+    printf("%15s%5s%5s","st","lb","oz");
+    printf("\n===============================\n");
+    printf("%10s%5d%5d%5d","Weight1",stone1,lb1,oz1);
+    printf("\n%10s%5d%5d%5d","Weight2",stone2,lb2,oz2);
+    printf("\n===============================\n");
+    int totaloz;
+    float avgst,avglb,avgoz;
+    totaloz=oz1+(lb1*16)+(stone1*224)+oz2+(lb2*16)+(stone2*224);
+    printf("%10s%5d%5d%5d","Sum",totaloz/224,(totaloz%224)/16,(totaloz%224)%16);
+    avgst=(totaloz/224)*(0.5);
+    avglb=((totaloz%224)/16)*(0.5);
+    avgoz=((totaloz%224)%16)*(0.5);
+    printf("\n%10s%5.1f%5.1f%5.1f","Average",avgst,avglb,avgoz);
     return 0;
 }
