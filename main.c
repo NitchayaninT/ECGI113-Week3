2210af075fff4aef7cbcd2c74af35e12159af46f
NitchayaninT <146054441+NitchayaninT@users.noreply.github.com>
2023-09-27 11:45:10 +0700

    Lab week 3

--- main.c
+++ main.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main()
 {
-    int stone1,lb1,oz1,stone2,lb2,oz2;
-    printf("Input 1st weight:");
-    scanf("%d,%d,%d",&stone1,&lb1,&oz1);
-    printf("Input 2nd weight:");
-    scanf("%d,%d,%d",&stone2,&lb2,&oz2);
-    printf("%15s%5s%5s","st","lb","oz");
-    printf("\n===============================\n");
-    printf("%10s%5d%5d%5d","Weight1",stone1,lb1,oz1);
-    printf("\n%10s%5d%5d%5d","Weight2",stone2,lb2,oz2);
-    printf("\n===============================\n");
-    int totaloz;
-    float avgst,avglb,avgoz;
-    totaloz=oz1+(lb1*16)+(stone1*224)+oz2+(lb2*16)+(stone2*224);
-    printf("%10s%5d%5d%5d","Sum",totaloz/224,(totaloz%224)/16,(totaloz%224)%16);
-    avgst=(totaloz/224)*(0.5);
-    avglb=((totaloz%224)/16)*(0.5);
-    avgoz=((totaloz%224)%16)*(0.5);
-    printf("\n%10s%5.1f%5.1f%5.1f","Average",avgst,avglb,avgoz);
+    int a,b,c;
+    printf("Input a,b,c:");
+    scanf("%d,%d,%d",&a,&b,&c);
+    float x1,x2,x;
+    x1=(-b+sqrt(pow(b,2)-(4*a*c)))/(2*a);
+    x2=(-b-sqrt(pow(b,2)-(4*a*c)))/(2*a);
+
+    if(pow(b,2)-(4*a*c)<0)//case 1
+    {
+        printf("no solutions\n");
+    }
+    else if(a==0)
+    {
+        if(b==0)
+        {
+          printf("no solutions\n");
+        }
+        else{
+        x=(-c)/b;
+        printf("x=%.1f\n",x);
+        }
+    }
+    else if(x1==x2)
+    {
+        printf("There is a single solution which is %d\n",x1);
+    }
+   else if(x1&&x2>0||x1&&x2<0)
+    {
+       printf("x1 = %.1f and x2 = %.1f",x1,x2);
+    }
+   else{
+    printf("no solutions\n");
+   }
     return 0;
 }
